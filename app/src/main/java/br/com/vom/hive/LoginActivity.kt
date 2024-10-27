3098517690f5449b6a10c710d161335c622e903e
gutolive09 <gutolive01@gmail.com>
2024-10-27 19:33:40 -0300

    Primeiro commit nesse repo, Main/ Login refeitos

---
+++ app/src/main/java/br/com/vom/hive/LoginActivity.kt
@@ -0,0 +1,29 @@
+package br.com.vom.hive
+
+import android.content.Intent
+import android.os.Bundle
+import android.text.SpannableString
+import android.text.style.UnderlineSpan
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login)
+        window.statusBarColor = getColor(R.color.mainOrange)
+
+        val loginButton = findViewById<TextView>(R.id.loginButton)
+        loginButton.setOnClickListener {
+            Toast.makeText(this, "Logado com sucesso!", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
